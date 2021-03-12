201dc6494e49a1e2000687f41a6a342921f00f85
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-12 09:24:34 -0800

    finally got the index in models to pull everything together and got the foreignkey connected

--- models/index.js
+++ models/index.js
@@ -1,20 +1,34 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: "category_id",
+});
 
 // Categories have many Products
+Categories.hasMany(Product, {
+    foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+    through: ProductTag,
+    foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+    through: ProductTag,
+    foreignKey: "tag_id",
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
